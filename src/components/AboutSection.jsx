62afd56667258db599def925b2b46336009d8630
Tumbikani Thomas Kapandamake <kapandathomas0@gmail.com>
2025-09-29 17:25:51 +0200

    added a download resume feature

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,14 @@ export const AboutSection = () => {
                         </p>
                                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                         <a href="#contact" className="cosmic-button"> Get In Touch</a>
-                        <a href="#contact" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"> Download CV</a>
+                        <a 
+                          href="/ThomasKapanda_Resume.pdf" 
+                        download="ThomasKapanda_Resume.pdf" 
+                         className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                        >
+                         Download Resume
+                        </a>
+
                     </div>
                     </div>
 
